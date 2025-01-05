cb6166a963f8fd587d5adb79be3825d1876f2086
s0600204 <dev@s0600204>
2025-01-05 00:52:53 +0000

    Move the splitting of received messages into `NetworkDevice`
    So it can use the correct text decoding and separator characters for the device

--- py/shure/networkdevice.py
+++ py/shure/networkdevice.py
@@ -1,3 +1,4 @@
+import logging
 
 from networkdevice import NetworkDevice
 
@@ -15,3 +16,27 @@ class ShureNetworkDevice(NetworkDevice):
         'uhfr': WirelessUHFRMic,
         'ulxd': WirelessQULXDMic,
     }
+
+    def parse_raw_rx(self, data):
+        data = data.strip('< >').strip('* ')
+        data = data.replace('{', '').replace('}', '')
+        data = data.rstrip()
+        split = data.split()
+        if data:
+            try:
+                if split[0] in ['REP', 'REPORT', 'SAMPLE'] and split[1] in ['1', '2', '3', '4']:
+                    ch = self.get_device_by_channel(int(split[1]))
+                    ch.parse_raw_ch(data)
+
+                elif split[0] in ['REP', 'REPORT']:
+                    self.raw[split[1]] = ' '.join(split[2:])
+            except:
+                logging.warning("Index Error(RX): %s", data)
+
+    def split_raw_rx(self, data):
+        data = data.decode(self.ENCODING)
+        if self.type == 'uhfr':
+            return [entry for entry in data.split("*") if entry]
+
+        sep = '>'
+        return [entry + sep for entry in data.split(sep) if entry]
