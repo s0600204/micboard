d9cf31cf13efc7c6e7c637ed58ae26998d074008
s0600204 <dev@s0600204>
2025-01-11 22:53:19 +0000

    Move the Wireless Queue Query Interval value to util.py

--- py/util.py
+++ py/util.py
@@ -1,9 +1,14 @@
 import enum
 import socket
 
+
+WIRELESS_QUERY_QUEUE_INTERVAL = 10 # seconds
+
+
 class NetworkProtocol(enum.Enum):
     TCP = socket.IPPROTO_TCP
     UDP = socket.IPPROTO_UDP
+
 
 def TVLookup(frequency):
     frequency = float(frequency)
