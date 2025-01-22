e0178b2ef35c6ce581e82563db6a8d4b03e0dc73
s0600204 <dev@s0600204>
2025-01-22 19:44:15 +0000

    Implement Sennheiser device discovery

--- py/micboard.py
+++ py/micboard.py
@@ -6,6 +6,7 @@ import time
 import config
 import tornado_server
 import device_manager
+from sennheiser import discover as sennheiser_discover
 from shure import discover as shure_discover
 
 
@@ -18,12 +19,14 @@ def main():
     rxquery_t = threading.Thread(target=device_manager.WirelessQueryQueue)
     rxcom_t = threading.Thread(target=device_manager.SocketService)
     web_t = threading.Thread(target=tornado_server.twisted)
+    discover_sennheiser_t = threading.Thread(target=sennheiser_discover.discover)
     discover_shure_t = threading.Thread(target=shure_discover.discover)
     rxparse_t = threading.Thread(target=device_manager.ProcessRXMessageQueue)
 
     rxquery_t.start()
     rxcom_t.start()
     web_t.start()
+    discover_sennheiser_t.start()
     discover_shure_t.start()
     rxparse_t.start()
 
